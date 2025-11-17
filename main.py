500ce5efa89ae7ad45ba93c9f9d09d91ba3dbb71
Ahmed Khalid <khalid.a5251@gmail.com>
2025-11-18 00:02:14 +0200

    u

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ with col2:
 
 with col3:
 
-    time.sleep(15)
+    time.sleep(11)
     def img_to_base64(path):
         with open(path, "rb") as f:
             return base64.b64encode(f.read()).decode()
